866c48436de448bb575161b872aae5409f80784f
Bbokkie-recipe <rbbokkie@gmail.com>
2022-04-30 16:55:26 +0900

    categoryUrl

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 let news = [];
+let menus = document.querySelectorAll(".menus button");
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByTopic(event))
+);
 const getLatestNews = async () => {
   let url = new URL(
     `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=5`
   );
   let header = new Headers({
-    "x-api-key": "",
+    "x-api-key": "mq7AfahUuQLPwY4_WhXjc9YCjgQKkKz6KhZvMbEDthE",
   });
   let response = await fetch(url, { headers: header });
   let data = await response.json();
@@ -13,23 +17,34 @@ const getLatestNews = async () => {
   render();
 };
 
+const getNewsByTopic = (event) => {
+  console.log("클림됨", event.target.textContent);
+  let topic = event.target.textContent.toLowerCase();
+  let url = new URL(
+    `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${topic}&page_size=5`
+  );
+  console.log("uuu", url);
+};
+
 const render = () => {
   let newsHTML = "";
-  newsHTML = news.map((news) => {
-    return `<div class="row news">
+  newsHTML = news
+    .map((newsitem) => {
+      return `<div class="row news">
     <div class="col-lg-4">
       <img
         class="news-img-size"
-        src="https://s3.ap-northeast-2.amazonaws.com/elasticbeanstalk-ap-northeast-2-176213403491/media/magazine_img/magazine_302/104_%E1%84%8A%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF.jpg"
+        src="${newsitem.media}"
       />
     </div>
     <div class="col-lg-8">
-      <h2>오드아이 고양이</h2>
-      <p>고양이가 세상을 구했다고 한다</p>
-      <div>출처 : 내 마음</div>
+      <h4>${newsitem.title}</h4>
+      <p>${newsitem.summary}</p>
+      <div>${newsitem.rights}*${newsitem.published_date}</div>
     </div>
   </div>`;
-  });
+    })
+    .join("");
 
   document.getElementById("news-board").innerHTML = newsHTML;
 };
